67946905742db5bef2871bc98f0ac73ba4bec29b
Florin Ionescu <sii76354@adobe.com>
2020-02-14 11:17:35 +0200

    Routes added

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 // MAIN ENTRY FILE
 
 // 1. bring express
-const express = require('express');
-// db route connection
-const connectDB = require('./config/db')
+const express = require("express");
+// 5.1 db route connection
+const connectDB = require("./config/db");
 
 // 2. use express
 const app = express();
 
-// db connection
+// 5.2 db connection
 connectDB();
 
 // 4. dev code ---------------
 
-    app.get('/', (req, res) => res.send(`API RUNNING`))
+app.get("/", (req, res) => res.send(`API RUNNING`));
 
 // ---------------------------
+
+// 6. define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // 3. port and env variable config
 const PORT = process.env.PORT || 5000;
