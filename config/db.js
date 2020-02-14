a373ac011d7d276ee142acb363d4dea09627d17a
Florin Ionescu <sii76354@adobe.com>
2020-02-14 13:50:40 +0200

    Implementing User Registration

--- config/db.js
+++ config/db.js
@@ -6,12 +6,13 @@ const db = config.get("mongoURI");
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
     });
     console.log("MongoDB Connected...");
   } catch (err) {
-    console.error(err.message);
+    console.error("Mongo", err.message);
 
     // Exit process with failure
     process.exit(1);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+// import model
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    Register user
@@ -17,15 +21,52 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     // in order to work 'req.body' we need to use bodyParser
     // logs the data that we send
     // console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array()});
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Route");
+
+    const { name, email, password } = req.body;
+
+    // make a query with mongoose
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      // create an instance of an user
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      // save the user into the database
+      await user.save();
+
+      // Return JWT
+      // Now the reason I'm returning the JWT token is because in the front end when a user registers I want them to get logged in right away
+      res.send("User Registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
